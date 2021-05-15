a80b7a64aa06411a502e82691e4143d817230611
Nikhil Patel <napatel@usc.edu>
2021-05-14 19:56:48 -0700

    Websocket broadcasts colors, but not to specific rooms

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def create_room():
     while room_number in rooms.keys():
         room_number = randint(0, 1000000)
     
-    rooms[room_number]=Room(cpm, colors)
+    rooms[room_number]=Room(room_number, cpm, colors)
 
     return {
         "room_number": room_number
@@ -52,16 +52,33 @@ def on_join(data):
     print(rooms)
     print()
     room = rooms[int(room_number)]
+    
+    
 
     join_room(room_number)
+    #message(room_number, '748723874')
 
-    message(room_number)
-    
+    if(room.dead):
+        room.dead=False
+        x = threading.Thread(target=startRave, args=(room,))
+        x.start()
+
     print("joined room"+(str)(room_number))
 
 @socketio.on("colorChange")
-def message(room_number):
-    socketio.emit("colorChange", "colordoisidfiosChange", room=room_number)
+def message(room_number, color):
+    print("emitting")
+    socketio.emit("colorChange", color, broadcast=True)
+
+@socketio.on("colorChange")
+def startRave(room):
+    i = 0
+    while not room.dead:
+        print(room.room_number)
+        message(room.room_number, room.colors[i%len(room.colors)])
+        i+=1
+        #TODO real timing
+        time.sleep(1)
 
 #TODO
 # # Supposed to catch 404 errors
