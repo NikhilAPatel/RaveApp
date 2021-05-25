1fafca825149c216721947c03e9286d7e8b7c738
Nikhil Patel <napatel@usc.edu>
2021-05-25 13:28:48 -0700

    Added spotify auth

--- app.py
+++ app.py
@@ -1,22 +1,56 @@
 from flask import (
     Flask,
     render_template,
-    request
+    request,
+    redirect,
+    g
 )
 from flask_cors import CORS
 from flask_socketio import (SocketIO, join_room, leave_room)
 from random import(seed, randint)
 from room import Room
 import threading
+import requests
 import time
 from datetime import datetime
 import json
+from urllib.parse import quote
+
 
 
 # Create the application instance
 app = Flask(__name__, template_folder="templates")
-socketio = SocketIO(app)
-CORS(app)
+#socketio = SocketIO(app)
+#CORS(app)
+
+#  Client Keys
+CLIENT_ID = "8457ff0a5bd847ccbb7b04886fd1bdf1"
+CLIENT_SECRET = "85a731c048a34a97ba78b1937193057a"
+
+# Spotify URLS
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+API_VERSION = "v1"
+SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
+
+# Server-side Parameters
+CLIENT_SIDE_URL = "http://127.0.0.1"
+PORT = 5000
+REDIRECT_URI = "{}:{}/callback/q".format(CLIENT_SIDE_URL, PORT)
+SCOPE = "playlist-modify-public playlist-modify-private"
+STATE = "code"
+SHOW_DIALOG_bool = True
+SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+
+auth_query_parameters = {
+    "response_type": "code",
+    "redirect_uri": REDIRECT_URI,
+    "scope": SCOPE,
+    # "state": STATE,
+    # "show_dialog": SHOW_DIALOG_str,
+    "client_id": CLIENT_ID
+}
 
 
 # Create a URL route in our application for "/"
@@ -102,6 +136,49 @@ def join_rave():
 def rave(room_number):
     return render_template('rave.html')
 
+@app.route("/login")
+def login():
+    # Auth Step 1: Authorization
+    url_args = "&".join(["{}={}".format(key, quote(val)) for key, val in auth_query_parameters.items()])
+    auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
+    return redirect(auth_url)
+
+@app.route("/callback/q")
+def callback():
+    # Auth Step 4: Requests refresh and access tokens
+    auth_token = request.args['code']
+    code_payload = {
+        "grant_type": "authorization_code",
+        "code": str(auth_token),
+        "redirect_uri": REDIRECT_URI,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+    }
+    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload)
+
+    # Auth Step 5: Tokens are Returned to Application
+    response_data = json.loads(post_request.text)
+    access_token = response_data["access_token"]
+    refresh_token = response_data["refresh_token"]
+    token_type = response_data["token_type"]
+    expires_in = response_data["expires_in"]
+
+    # Auth Step 6: Use the access token to access Spotify API
+    authorization_header = {"Authorization": "Bearer {}".format(access_token)}
+
+    # Get profile data
+    user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
+    profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
+    profile_data = json.loads(profile_response.text)
+
+    # Get user playlist data
+    playlist_api_endpoint = "{}/playlists".format(profile_data["href"])
+    playlists_response = requests.get(playlist_api_endpoint, headers=authorization_header)
+    playlist_data = json.loads(playlists_response.text)
+
+    # Combine profile and playlist data to display
+    display_arr = [profile_data] + playlist_data["items"]
+    return render_template("loggedIn.html", sorted_array=display_arr)
 
 # Redirect 404s to the home page
 @app.errorhandler(404)
